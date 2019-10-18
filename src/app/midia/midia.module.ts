e31b2173de4562b191fba4cc446f950b05da138d
DeleonxD <deleonsimoni@gmail.com>
2019-10-17 22:48:37 -0300

    correções

--- src/app/midia/midia.module.ts
+++ src/app/midia/midia.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-
 import { MidiaRoutingModule } from './midia-routing.module';
 import { MidiaComponent } from './midia.component';
+import { NgxTwitterTimelineModule } from 'ngx-twitter-timeline';
+import { ngtweet } from 'ngtweet';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, MidiaRoutingModule],
+  imports: [CommonModule, TranslateModule, MidiaRoutingModule, NgxTwitterTimelineModule],
   declarations: [MidiaComponent]
 })
-export class MidiaModule {}
+export class MidiaModule { }
