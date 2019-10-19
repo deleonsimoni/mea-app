e57e37e8d9319da07f93c4b75ffcdb7150e70cd2
DeleonxD <deleonsimoni@gmail.com>
2019-10-18 23:40:54 -0300

    acertos menu

--- src/app/core/authentication/authentication.guard.ts
+++ src/app/core/authentication/authentication.guard.ts
@@ -10,15 +10,16 @@ const log = new Logger('AuthenticationGuard');
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
-  constructor(private router: Router, private credentialsService: CredentialsService) {}
+  constructor(private router: Router, private credentialsService: CredentialsService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (this.credentialsService.isAuthenticated()) {
+    return true;
+    /*if (this.credentialsService.isAuthenticated()) {
       return true;
     }
 
     log.debug('Not authenticated, redirecting and adding redirect url...');
     this.router.navigate(['/login'], { queryParams: { redirect: state.url }, replaceUrl: true });
-    return false;
+    return false;*/
   }
 }
